4a2039a4ad992e7f6f1c566a99ea7ea9077637e9
kookie-dev <cjk921211@gmail.com>
2023-12-14 17:46:06 +0900

    메인 화면 작업 중

---
+++ APP3_LMessenger/beginning/LMessenger/LMessenger/View/Chat/ChatListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListView.swift
+//  LMessenger
+//
+//  Created by Choi Oliver on 12/14/23.
+//
+
+import SwiftUI
+
+struct ChatListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatListView()
+}
---
+++ APP3_LMessenger/beginning/LMessenger/LMessenger/View/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  LMessenger
+//
+//  Created by Choi Oliver on 12/14/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
